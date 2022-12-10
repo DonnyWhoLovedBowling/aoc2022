4ba2d2bf85a4cf2059b46ae3a558e976c0729eee
DonnyWhoLovedBowling <p.c.van.der.deijl@gmail.com>
2022-12-10 10:02:06 +0100

    ex10

---
+++ src/ex10.py
@@ -0,0 +1,33 @@
+f = open('../data/ex10.txt')
+lines = f.readlines()
+
+cycle = 1
+register = 1
+score = 0
+score_cycles = [20, 60, 100, 140, 180, 220]
+sprite = [0, 1, 2]
+CRT = ''
+
+for line in lines:
+    line = line.replace('\n', '')
+    old_cycle = cycle
+    old_register = register
+    if 'noop' in line:
+        cycle += 1
+    else:
+        cycle += 2
+        register += int(line.split(' ')[1])
+    for i in range(old_cycle, cycle):
+        if i in score_cycles:
+            score += (i*old_register)
+            break
+    for i in range(old_cycle, cycle):
+        if ((i-1) % 40) in sprite:
+            CRT += '#'
+        else:
+            CRT += '.'
+    sprite = [register+i for i in range(-1, 2, 1)]
+
+print(score)
+for i in range(0, len(CRT), 40):
+    print(CRT[i:i+40])
